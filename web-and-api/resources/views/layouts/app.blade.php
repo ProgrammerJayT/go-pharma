a9ce6703f2a92dcf89b7ad1501e3f59cecf2b237
Programmer JayT <theanthem8@gmail.com>
2022-12-17 14:45:13 +0200

    Added hotwiredturbo for SPA and native feel

--- web-and-api/resources/views/layouts/app.blade.php
+++ web-and-api/resources/views/layouts/app.blade.php
@@ -12,9 +12,24 @@
 </head>
 
 <body>
-    {{ $slot }}
+    <div class="min-h-full">
+        @include('partials.navbar')
+        @include('partials.header')
+
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
 </body>
 
 @livewireScripts
+<script type="module">
+    import hotwiredTurbo from 'https://cdn.skypack.dev/@hotwired/turbo';
+</script>
+
+<script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js"
+    data-turbolinks-eval="false" data-turbo-eval="false"></script>
+<script src="https://unpkg.com/flowbite@1.5.5/dist/flowbite.js"></script>
+
 
 </html>
