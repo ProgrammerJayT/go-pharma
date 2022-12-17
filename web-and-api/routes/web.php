3785291f8763da299e43a6175489a9c0b8ffc8f9
Programmer JayT <theanthem8@gmail.com>
2022-12-17 12:05:59 +0200

    Created dashboard route and fallback route

--- web-and-api/routes/web.php
+++ web-and-api/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Shared\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', Dashboard::class);
+
+//Create fallback route to dashboard
+Route::fallback(function () {
+    return redirect('/dashboard');
 });
